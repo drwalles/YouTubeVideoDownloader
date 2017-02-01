6f6d9b69c3669cb0406f8dbb0211dc0286352130
drwalles <aminulhaquerasel@gmail.com>
2017-02-01 21:02:42 +0600

    A simple Youtube video downloader
    A simple Youtube video downloader
    to download youtube video in any quality.

---
+++ YouTubeVideoDownloader/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExtractor;
+using System.IO;
+using System.Threading;
+
+namespace YouTubeVideoDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            IEnumerable<VideoInfo> videos = DownloadUrlResolver.GetDownloadUrls(textBox1.Text);
+            VideoInfo video = videos.First(p => p.VideoType == VideoType.Mp4 && p.Resolution == Convert.ToInt32(comboBox1.Text));
+            if (video.RequiresDecryption)
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            VideoDownloader downloader = new VideoDownloader(video, Path.Combine(Application.StartupPath + "\\", video.Title + video.VideoExtension));
+            downloader.DownloadProgressChanged += Downloader_DownloadProgressChanged;
+            Thread thread = new Thread(() => { downloader.Execute(); }) { IsBackground = true };
+            thread.Start();
+        }
+
+        private void Downloader_DownloadProgressChanged(object sender, ProgressEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate ()
+            {
+                progressBar1.Value = (int)e.ProgressPercentage;
+                label3.Text = $"{String.Format("{0:0.##}", e.ProgressPercentage)}%";
+                progressBar1.Update();
+            }));
+            //throw new NotImplementedException();
+        }
+    }
+}
